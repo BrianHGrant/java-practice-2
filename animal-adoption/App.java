0aff3753ca0e00a6fcfa72818766a1d0aa687584
BrianHGrant <bhgrant8@gmail.com>
2016-12-17 22:22:05 -0800

    add ui for animal adoption

---
+++ animal-adoption/App.java
@@ -0,0 +1,30 @@
+public class App {
+  public static void main(String[] args) {
+    Animal bobby = new Animal();
+      bobby.mId = 1;
+      bobby.mName = "Bobby";
+      bobby.mSpecies = "walrus";
+      bobby.mColor = "grey";
+      bobby.mAge = 75;
+
+
+    Animal phil = new Animal();
+      phil.mId = 2;
+      phil.mName = "Phil";
+      phil.mSpecies = "giraffe";
+      phil.mColor = "orange";
+      phil.mAge = 32;
+
+
+    Animal[] allAnimals = {bobby, phil};
+
+    for ( Animal animal : allAnimals) {
+      System.out.println( "----------------------" );
+      System.out.println(animal.mId);
+      System.out.println(animal.mName);
+      System.out.println(animal.mSpecies);
+      System.out.println(animal.mColor);
+      System.out.println(animal.mAge);
+    }
+  }
+}
